34a9d19ab633c7aaf29038b5bac94e70a864929f
Yongyi <yongyi781@hotmail.com>
2015-12-01 16:24:54 -0500

    Added my own FromServicesAttribute so that it can be used on properties

---
+++ src/GOP/FromServices.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GOP
+{
+    /// <summary>
+    /// Specifies that a controller action parameter, controller property, or model property should be bound using the request services during model binding.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+    public class FromServicesAttribute : Microsoft.AspNet.Mvc.FromServicesAttribute
+    {
+    }
+}
