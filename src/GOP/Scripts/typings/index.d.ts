2fe091f52c637ab2e13520e5abb368b64c5c8348
Yongyi <yongyi781@hotmail.com>
2017-01-06 17:26:13 -0500

    Replaced typings with npm @types, and other small changes

--- src/GOP/Scripts/typings/index.d.ts
+++ src/GOP/Scripts/typings/index.d.ts
@@ -1,7 +1,1 @@
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/knockout/index.d.ts" />
-/// <reference path="globals/qunit/index.d.ts" />
 /// <reference path="globals/signalr/index.d.ts" />
-/// <reference path="globals/stats/index.d.ts" />
-/// <reference path="globals/three-orbitcontrols/index.d.ts" />
-/// <reference path="globals/three/index.d.ts" />
